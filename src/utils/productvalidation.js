cf44c366acb7e5b2914a9cd70d90cf083f7a3b27
Adebafnjo Israel <debanjo31@gmail.com>
2023-09-27 14:19:58 +0100

    joi validate

--- src/utils/productvalidation.js
+++ src/utils/productvalidation.js
@@ -1,16 +1,22 @@
 import Joi from "joi";
 
-export const productSchemas = { 
-
-    productcreation: Joi.object().keys({ 
-        productName: Joi.string().required(),
-        productPrice: Joi.number(),
-        productDescription: Joi.string().required(),
-        adminId: Joi.string().required(),
-        productImage:  Joi.string().required(),
-    })
-}
+export const productcreation = Joi.object().keys({ 
+    productName: Joi.string().required(),
+    productPrice: Joi.number(),
+    productDescription: Joi.string().required(),
+    adminId: Joi.string().required(),
+    productImage:  Joi.string().required(),
+})
 
 
 
+// export const productSchemas = { 
 
+//     productcreation: Joi.object().keys({ 
+//         productName: Joi.string().required(),
+//         productPrice: Joi.number(),
+//         productDescription: Joi.string().required(),
+//         adminId: Joi.string().required(),
+//         productImage:  Joi.string().required(),
+//     })
+// }
