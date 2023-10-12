09a9fa9511dfbeeb1673452ba82e6ed7a9ccbfea
opeyemibioku <opeyemibioku@gmail.com>
2023-10-12 15:36:29 +0100

    updates

--- src/routers/auths.js
+++ src/routers/auths.js
@@ -1,16 +1,27 @@
 import express from "express";
-import { companySignup, companyLogin, lawyerLogin, lawyerSignup, adminLogin, adminSignup, confirmEmail, forgotPassword, resetPassword, resetPasswordToken} from "../controllers/authcontrollers.js";
- import {routeBasedOnUserType } from './../controllers/middleware.js'
+import {
+  companySignup,
+  companyLogin,
+  lawyerLogin,
+  lawyerSignup,
+  adminLogin,
+  adminSignup,
+  confirmEmail,
+  forgotPassword,
+  resetPassword,
+  resetPasswordToken,
+} from "../controllers/authcontrollers.js";
+import { routeBasedOnUserType } from "./../controllers/middleware.js";
 
 const router = express.Router();
 
-router.post('/login/:userType', routeBasedOnUserType);
+router.post("/login/:userType", routeBasedOnUserType);
 router.post("/company/signup", companySignup);
 router.post("/lawyer/signup", lawyerSignup);
 router.post("/admin/signup", adminSignup);
-router.get("/useremail/confirm/:token", confirmEmail)
-router.post('/forgot-password/:userType', forgotPassword);
+router.get("/useremail/confirm/:token", confirmEmail);
+router.post("/forgot-password/:userType", forgotPassword);
 router.post("/confirm-reset-token", resetPasswordToken);
-router.post('/reset-password', resetPassword);
+router.post("/reset-password", resetPassword);
 
 export default router;
