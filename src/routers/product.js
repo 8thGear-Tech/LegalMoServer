8fbe68e1c8de7070ad2dfb326673661697248ee2
opeyemibioku <opeyemibioku@gmail.com>
2023-10-16 16:32:14 +0100

    updated the product controller

--- src/routers/product.js
+++ src/routers/product.js
@@ -7,11 +7,24 @@ import {
   singleProduct,
 } from "../controllers/productcontroller.js";
 import { authToken } from "../middleware/AuthToken.js";
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
 const router = express.Router();
 
 // router.post("/api/create",middleware(productSchemas.productcreation), create);
 // router.post("/api/create", authToken, create);
-router.post("/api/create", create);
+router.post("/api/create", upload.single("productImage"), create);
 router.get("/api/products", getProducts);
 router.put("/api/update/:id", updateProduct);
 router.delete("/api/delete/:id", deleteProduct);
