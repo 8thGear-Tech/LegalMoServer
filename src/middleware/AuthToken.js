cda31843577ed7e6b5fa43e50d504f5d1cdb3862
opeyemibioku <opeyemibioku@gmail.com>
2023-10-18 11:41:55 +0100

    updated the payment endpoint

--- src/middleware/AuthToken.js
+++ src/middleware/AuthToken.js
@@ -1,17 +1,17 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
 export const authToken = (req, res, next) => {
-    const token = req.headers.authorization;
-    if(!token){
-        return res.status(401).json({message: "unauthorized access"})
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: "unauthorized access" });
+  }
+  jwt.verify(token, "this_is_legalmos_secret_it_aint_leaking", (err, user) => {
+    if (err) {
+      console.log(err);
+      return res.status(403).json({ message: "Forbidden" });
     }
-    jwt.verify(token, 'this_is_legalmos_secret_it_aint_leaking', (err, user) => {
-        if(err){
-            console.log(err)
-            return res.status(403).json({ message : 'Forbidden'})
-        }
 
-        req.user = user;
-        next()
-    })
-}
+    req.user = user;
+    next();
+  });
+};
