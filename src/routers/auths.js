130947768b2365e98f55ced08defde5fd87de95e
opeyemibioku <opeyemibioku@gmail.com>
2023-11-06 11:15:03 +0100

    updates

--- src/routers/auths.js
+++ src/routers/auths.js
@@ -1,13 +1,23 @@
 import express from "express";
-import { companySignup, lawyerSignup, adminSignup, logoutUser,usersLogin } from "../controllers/authcontrollers.js";
-import {confirmEmail} from "../utils/email.js"
-import { forgotPassword, resetPassword, resetPasswordToken } from "../controllers/passwords.js";
+import {
+  companySignup,
+  lawyerSignup,
+  adminSignup,
+  logoutUser,
+  usersLogin,
+} from "../controllers/authcontrollers.js";
+import { confirmEmail } from "../utils/email.js";
+import {
+  forgotPassword,
+  resetPassword,
+  resetPasswordToken,
+} from "../controllers/passwords.js";
 // import { usersLogin } from "../controllers/middleware.js";
 
 const router = express.Router();
 
 // Use a route parameter to set the userType based on the route
-router.post('/login', usersLogin);
+router.post("/login", usersLogin);
 router.post("/company/signup", companySignup);
 router.post("/lawyer/signup", lawyerSignup);
 router.post("/admin/signup", adminSignup);
