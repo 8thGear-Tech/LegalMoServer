991d07baf2687834e6f0f5834c7241d37af483c8
opeyemibioku <opeyemibioku@gmail.com>
2023-11-09 17:04:59 +0100

    updates

--- src/routers/usersrouters.js
+++ src/routers/usersrouters.js
@@ -15,7 +15,7 @@ const router = express.Router();
 
 // router.use(authenticateUser);
 router.get("/:userType/:userId", authenticateUser, profileBasedOnUserType);
-router.get("/lawyers", authenticateUser, isAdminUser, getAllLawyers);
+// router.get("/lawyers", authenticateUser, isAdminUser, getAllLawyers);
 router.get("/companies", authenticateUser, isAdminUser, getAllCompanies);
 router.get("/admins", authenticateUser, isAdminUser, getAllAdmins);
 router.patch("/updateprofile", authenticateUser, updateProfileBasedOnUser);
