f1eba0721236e15199455c1d0d6d16f0a7444881
opeyemibioku <opeyemibioku@gmail.com>
2023-11-09 15:54:47 +0100

    updated the cart endpoints

--- src/routers/cart.js
+++ src/routers/cart.js
@@ -1,15 +1,20 @@
 import express from "express";
-import { addToCart, getCart, deleteCart, clearCart, checkout, getAllCart } from "../controllers/cartcontroller.js";
+import {
+  addToCart,
+  getCart,
+  deleteCart,
+  clearCart,
+  checkout,
+  getAllCart,
+} from "../controllers/cartcontroller.js";
 import { authToken } from "../middleware/AuthToken.js";
 const router = express.Router();
 
-
-router.post('/api/cart', authToken, addToCart)
-router.get('/api/cart', authToken, getCart)
-router.delete('/api/cart/:id', authToken, deleteCart)
-router.post('/api/clear-cart', authToken, clearCart)
-router.post('/api/checkout', authToken, checkout)
-router.get('/api/allcart', authToken, getAllCart)
+router.post("/api/cart", addToCart);
+router.get("/api/cart", getCart);
+router.delete("/api/cart/:id", deleteCart);
+router.post("/api/clear-cart", clearCart);
+router.post("/api/checkout", authToken, checkout);
+router.get("/api/allcart", getAllCart);
 
 export default router;
-
