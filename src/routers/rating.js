09f05b7bcd7f039f38bb7b8d5f710d6b9b164627
opeyemibioku <opeyemibioku@gmail.com>
2023-11-13 21:30:57 +0100

    updates

--- src/routers/rating.js
+++ src/routers/rating.js
@@ -13,12 +13,12 @@ import { authToken } from "../middleware/AuthToken.js";
 const router = express.Router();
 
 router.post("/api/rating", authToken, addRating);
-router.get("/api/rating", getRatings);
+router.get("/ratings-api/rating", getRatings);
 router.get("/api/rating/:id", getRating);
 router.patch("/api/rating/:id", authToken, updateRating);
 router.delete("/api/rating/:id", authToken, deleteRating);
-router.get("/api/rating/company/:companyId", getRatingsByCompany);
-router.get("/api/rating/product/:productId", getRatingsByProduct);
-router.get("/api/rating/status/:status", getRatingsByStatus);
+router.get("/ratings-api/rating/company/:companyId", getRatingsByCompany);
+router.get("/ratings-api/rating/product/:productId", getRatingsByProduct);
+router.get("/ratings-api/rating/status/:status", getRatingsByStatus);
 
 export default router;
