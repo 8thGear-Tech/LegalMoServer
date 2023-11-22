914b833b4ae9e2d9701ae41e35ff3025c1e750d0
Adebafnjo Israel <debanjo31@gmail.com>
2023-11-22 12:48:54 +0100

    payment details

--- src/controllers/lawyercontroller.js
+++ src/controllers/lawyercontroller.js
@@ -4,63 +4,69 @@ import sendEmail from '../utils/email.js';
 
 
 export const addPaymentDetails = async (req, res) => {
-    const lawyerExists = await Lawyer.findById(req.userId)
-    if(!lawyerExists){
-        res.status(404).send({message : "Unauthorized!, You must be a lawyer"})
-        return
+  const lawyerExists = await Lawyer.findById(req.userId);
+  if (!lawyerExists) {
+    res.status(404).send({ message: 'Unauthorized!, You must be a lawyer' });
+    return;
+  }
+  const validate = paymentDetail.validate(req.body, options);
+  if (validate.error) {
+    const message = validate.error.details
+      .map((detail) => detail.message)
+      .join(',');
+    return res.status(400).send({
+      status: 'fail',
+      message,
+    });
+  }
+  const { accountNumber, accountName, bank } = req.body;
+  try {
+    const isLawyerMailConfirmed = await Lawyer.findById(req.userId);
+    if (!isLawyerMailConfirmed.isEmailConfirmed) {
+      res
+        .status(401)
+        .send({ message: 'Unauthorized!, Lawyer has not confirmed email' });
+      return;
     }
-    const validate = paymentDetail.validate(req.body, options)
-        if (validate.error) {
-            const message = validate.error.details.map((detail) => detail.message).join(',');
-                return res.status(400).send({
-                    status: 'fail',
-                    message,
-                })
-          }
-    const { accountNumber, accountName, bank } = req.body;
-    try {
-        const isLawyerMailConfirmed = await Lawyer.findById(req.userId)
-        if(!isLawyerMailConfirmed.isEmailConfirmed){
-            res.status(401).send({message : "Unauthorized!, Lawyer has not confirmed email"})
-            return
-        }
-        const lawyer = await Lawyer.findByIdAndUpdate(req.userId, {
-        $push: {
-            accountDetails: {
-            accountNumber: accountNumber,
-            accountName: accountName,
-            bank: bank,
-            },
+    const lawyer = await Lawyer.findByIdAndUpdate(req.userId, {
+      $push: {
+        accountDetails: {
+          accountNumber: accountNumber,
+          accountName: accountName,
+          bank: bank,
         },
-        });
-        res.status(200).json({ lawyer });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-    };
+      },
+    });
+    const lawyerUpdate = await Lawyer.findById(req.userId);
+    res.status(200).json({ lawyerUpdate });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 export const editPaymentDetails = async (req, res) => {
-    const lawyerExists = await Lawyer.findById(req.userId)
-    if(!lawyerExists){
-        res.status(404).send({message : "Unauthorized!, You must be a lawyer"})
-        return
-    }
-    const { accountNumber, accountName, bank } = req.body;
-    try {
-        const lawyer = await Lawyer.findByIdAndUpdate(req.userId, {
-        $set: {
-            accountDetails: {
-                accountNumber: accountNumber,
-                accountName: accountName,
-                bank: bank,
-                },
+  const lawyerExists = await Lawyer.findById(req.userId);
+  if (!lawyerExists) {
+    res.status(404).send({ message: 'Unauthorized!, You must be a lawyer' });
+    return;
+  }
+  const { accountNumber, accountName, bank } = req.body;
+  try {
+    const lawyer = await Lawyer.findByIdAndUpdate(req.userId, {
+      $set: {
+        accountDetails: {
+          accountNumber: accountNumber,
+          accountName: accountName,
+          bank: bank,
         },
-        });
-        res.status(200).json({ lawyer });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-    }
+      },
+    });
+    const lawyerUpdate = await Lawyer.findById(req.userId);
+    res.status(200).json({ lawyerUpdate });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 export const sendOTP = async (req, res) => {
     const lawyerExists = await Lawyer.findById(req.userId)
