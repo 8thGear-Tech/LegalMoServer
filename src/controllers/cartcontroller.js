89c002bb1030077d561f31c2a4244325ca1ebb01
Adebafnjo Israel <debanjo31@gmail.com>
2023-11-29 21:58:19 +0100

    Fix cart deletion and checkout functionality

--- src/controllers/cartcontroller.js
+++ src/controllers/cartcontroller.js
@@ -126,7 +126,8 @@ export const getCart = async (req, res) => {
     const cart = await Cart.find({ companyId: req.userId }).populate(
       'companyId products.productId'
     );
-    if (cart === null || cart.length == undefined) {
+    console.log(cart.length);
+    if (cart === null || cart.length == undefined || cart.length == 0) {
       res
         .status(400)
         .send('Nothing in the Cart, Pls add some products to your cart');
@@ -140,95 +141,103 @@ export const getCart = async (req, res) => {
 };
 
 export const deleteCart = async (req, res) => {
-    const companyExists = await Company.findById(req.userId)
-    if(!companyExists){
-        res.status(404).send({message : "Unauthorized!, You must be a company"})
-        return
+  const companyExists = await Company.findById(req.userId);
+  if (!companyExists) {
+    res.status(404).send({ message: 'Unauthorized!, You must be a company' });
+    return;
+  }
+  const productId = req.params.id;
+  const companyId = req.userId;
+  try {
+    console.log('Cart finding');
+    let cart = await Cart.findOne({ companyId });
+    if (!cart) {
+      res.status(404).send('Cart not found');
+      return;
     }
-    const productId  = req.params.id;
-    const companyId = req.userId
-    try {
-        console.log("Cart finding")
-        let cart = await Cart.findOne({companyId})
-        if(!cart){
-            res.status(404).send("Cart not found")
-            return
-        }
-        let products = cart.products
-        console.log(products)
-        const productIndex = products.findIndex((product)=>{
-            console.log(product.productId.toHexString())
-            console.log(productId)
-            return product.productId.toHexString() == productId
-        })
-        console.log(productIndex)
-        if(productIndex > -1){
-            let product = cart.products[productIndex]
-            console.log(product)
-            cart.bill -= product.quantity * product.price
-            if(cart.bill < 0){
-                cart.bill = 0
-            }
-            cart.products.splice(productIndex, 1)
-            cart.bill = cart.products.reduce((acc, curr) => {
-                return acc + curr.quantity * curr.price
-            }, 0)
-            cart = await cart.save()
-            res.status(200).send(cart)
-        }
-        else{
-            res.status(404).send("Product not found")
-        }
-    } catch (error) {
-        console.log(error)
-        res.status(500).send("something went wrong")
+    let products = cart.products;
+    console.log(products);
+    const productIndex = products.findIndex((product) => {
+      console.log(product.productId.toHexString());
+      console.log(productId);
+      return product.productId.toHexString() == productId;
+    });
+    console.log(productIndex);
+    if (productIndex > -1) {
+      let product = cart.products[productIndex];
+      console.log(product);
+      cart.bill -= product.quantity * product.price;
+      if (cart.bill < 0) {
+        cart.bill = 0;
+      }
+      cart.products.splice(productIndex, 1);
+      cart.bill = cart.products.reduce((acc, curr) => {
+        return acc + curr.quantity * curr.price;
+      }, 0);
+      cart = await cart.save();
+      res.status(200).send(cart);
+    } else {
+      res.status(404).send('Product not found');
     }
-}
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('something went wrong');
+  }
+};
 
 export const clearCart = async (req, res) => {
-    const companyExists = await Company.findById(req.userId)
-    if(!companyExists){
-        res.status(404).send({message : "Unauthorized!, You must be a company"})
-        return
-    }
-    const companyId  = req.userId;
-    try {
-        await Cart.deleteMany({companyId})
-        res.status(200).json({message: 'Cart cleared successfully'})
-    } catch (error) {
-        console.error(error)
-        res.status(500).send("something went wrong")
-    }
-}
+  const companyExists = await Company.findById(req.userId);
+  if (!companyExists) {
+    res.status(404).send({ message: 'Unauthorized!, You must be a company' });
+    return;
+  }
+  const companyId = req.userId;
+  try {
+    await Cart.deleteMany({ companyId });
+    res.status(200).json({ message: 'Cart cleared successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('something went wrong');
+  }
+};
 
 export const checkout = async (req, res) => {
-    const company = await Company.findById(req.userId)
-    if(!company){
-        res.status(404).send({message : "Unauthorized!, You must be a company"})
-        return
+  const company = await Company.findById(req.userId);
+  if (!company) {
+    res.status(404).send({ message: 'Unauthorized!, You must be a company' });
+    return;
+  }
+  const companyId = req.userId;
+  const cart = await Cart.findOne({ companyId });
+  console.log(cart);
+  try {
+    if (cart === null) {
+      res.status(400).send('Nothing in your cart');
+      return;
     }
-    const companyId  = req.userId;
-    const cart = await Cart.findOne({companyId})
-    console.log(cart.products)
-    try {
-        if(cart.products){
-            cart.products.forEach(product => {
-                const jobs = new Job({
-                    companyId : req.userId,
-                    productId: product.productId,  
-                })
-                jobs.save().then(() => {
-                    console.log(saved)
-                }).catch(err => {
-                    console.log(err)
-                })
-            })
-            await Cart.deleteMany({companyId})
-            await sendEmail({
-                email: company.officialEmail,
-                subject: 'Purchase Completed',
-                message: `Purchase Completed`,
-                html: `<p>Hello ${company.companyName}</p> 
+    if (cart.products) {
+      cart.products.forEach((product) => {
+        const jobs = new Job({
+          companyId: req.userId,
+          productId: product.productId,
+          companyDetail: product.detail,
+          companyFile: product.file,
+        });
+        jobs
+          .save()
+          .then(() => {
+            console.log('Job saved');
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
+      await Cart.deleteMany({ companyId });
+      await sendEmail({
+        email: company.officialEmail,
+        subject: 'Purchase Completed',
+        message: `Purchase Completed`,
+        html: `<p>Hello ${company.companyName}</p> 
                 <p>Thank your for placing an order with LegalMO. We are pleased to confirm the receipt of your order </p>
                 <p>Order details:</p>
                 <p>Item(s):  </p>
@@ -240,15 +249,13 @@ export const checkout = async (req, res) => {
                 <p>LegalMO</p>
                 <p>[Company contact details]</p>
                 <p></p>
-                `
-            });
-            return res.status(201).json("Checkout successful")
-        }
-        else{
-            res.status(400).send("Nothing in your cart")    
-        }
-        
-    } catch (error) {
-        res.status(500).send("something went wrong")       
-    } 
-}
+                `,
+      });
+      return res.status(201).json('Checkout successful');
+    } else {
+      res.status(400).send('Nothing in your cart');
+    }
+  } catch (error) {
+    res.status(500).send('something went wrong');
+  }
+};
