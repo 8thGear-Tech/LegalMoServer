0334396b530d3348b145a2c8677a81d3d10f0626
Adebanjo Israel <debanjo31@gmail.com>
2024-01-11 19:55:35 +0100

    multer uploaded allowed

--- app.js
+++ app.js
@@ -3,45 +3,49 @@ import morgan from "morgan";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import multer from "multer";
 import globalErrorHandler from "./src/utils/errorHandler.js";
 import config from "./src/config/index.js";
 import authRouter from "./src/routers/auths.js";
 import productRouter from "./src/routers/product.js";
-import cartRouter from "./src/routers/cart.js"
-import jobRouter from "./src/routers/job.js"
-import adminRouter from "./src/routers/admin.js"
-import ratingRouter from './src/routers/rating.js'
-import lawyerRouter from './src/routers/lawyer.js'
+import cartRouter from "./src/routers/cart.js";
+import jobRouter from "./src/routers/job.js";
+import adminRouter from "./src/routers/admin.js";
+import ratingRouter from "./src/routers/rating.js";
+import lawyerRouter from "./src/routers/lawyer.js";
 
 dotenv.config({ path: "./configenv.env" });
 
 const mongoURI = config.MONGODB_CONNECTION_URL;
 
 mongoose
-  .connect('mongodb+srv://legalmo:kQvhWA0S69hkOXTY@legalmo.n5ltuv1.mongodb.net/legalmodb?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://legalmo:kQvhWA0S69hkOXTY@legalmo.n5ltuv1.mongodb.net/legalmodb?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(console.log("Database connection is established"))
   .catch((err) => console.log(err.message));
 const port = config.PORT;
 const app = express();
 
-
 // Middleware
 app.use(morgan("tiny"));
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+const upload = multer();
+app.use(upload.array());
 
- app.use('/', authRouter);
- app.use('/', productRouter);
- app.use('/', cartRouter)
- app.use('/', jobRouter)
- app.use('/', adminRouter)
- app.use('/', ratingRouter)
- app.use('/', lawyerRouter)
-
+app.use("/", authRouter);
+app.use("/", productRouter);
+app.use("/", cartRouter);
+app.use("/", jobRouter);
+app.use("/", adminRouter);
+app.use("/", ratingRouter);
+app.use("/", lawyerRouter);
 
 app.use(
   cors({
